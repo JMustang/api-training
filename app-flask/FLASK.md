fcb3354e623183ad9d9a00eb238d95ec5321e817
Junior <eecfredes@gmail.com>
2023-01-24 21:07:20 -0300

    [python]: flask

---
+++ app-flask/FLASK.md
@@ -0,0 +1,13 @@
+# Flask
+
+- Ambiente de virtualização
+
+```bash
+ python3 -m venv venv     
+ ```
+
+ - Instalando o flask
+
+```bash
+pip install flask
+```
---
+++ app-flask/data.py
@@ -0,0 +1,26 @@
+Users = [
+    {
+        'id': 1,
+        'name': 'junior',
+        'email': 'email@gmail.com',
+        'working': True
+    },
+    {
+        'id': 2,
+        'name': 'alice',
+        'email': 'email@gmail.com',
+        'working': True
+    },
+    {
+        'id': 3,
+        'name': 'olaf',
+        'email': 'email@gmail.com',
+        'working': True
+    },
+    {
+        'id': 4,
+        'name': 'ragnar',
+        'email': 'email@gmail.com',
+        'working': True
+    },
+]
