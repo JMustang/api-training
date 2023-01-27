445ef379eb6ecfd4a674b976df3ece493386b3bd
Junior <eecfredes@gmail.com>
2023-01-26 21:48:07 -0300

    [python]: some improvements

--- app-flask/main.py
+++ app-flask/main.py
@@ -2,6 +2,7 @@ from flask import Flask, make_response, jsonify, request
 from data import Users
 
 app = Flask(__name__)
+app.config['JSON_SORT_KEY'] = False
 
 
 @app.route('/')
@@ -11,13 +12,24 @@ def get_main():
 
 @app.route('/users', methods=['GET'])
 def get_users():
-    return make_response(jsonify(Users))
+    return make_response(
+        jsonify(
+            message='User List.',
+            data=Users
+        )
+    )
 
 
 @app.route('/users', methods=['POST'])
 def create_user():
     user = request.json
-    return user
+    Users.append(user)
+    return make_response(
+        jsonify(
+            message='Users successfully added.',
+            data=user
+        )
+    )
 
 
 app.run()
